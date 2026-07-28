345fedd451f6426e438e5b644d7871737098a303
LuizDomingo27 <luiz-elayny@live.com>
2026-07-28 20:52:52 -0300

    Icon add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Tarefas da Equipe",
-    page_icon="◆",
+    page_icon="🎲",
     layout="wide",
     initial_sidebar_state="expanded",
 )
